a5502e9352d7fc349e9ce1e6cc74f9f38fc5ca0b
Davy Vanherbergen <davy.vanherbergen@gmail.com>
2016-11-05 17:47:02 +0100

    Add transmission of logmessages

--- RFEmitter.py
+++ RFEmitter.py
@@ -1,10 +1,12 @@
 import json
+import time
 from websocket import create_connection
 
 """ 
 Transmit all test execution events to a given target server. 
 """
 class RFEmitter:
+
     ROBOT_LISTENER_API_VERSION = 2
 
     def __init__(self, targetUrl, identifier):
@@ -13,33 +15,44 @@ class RFEmitter:
 
 
     def start_suite(self, name, attrs):
-        self.send('start_suite', name, attrs)
+        self.send('start_suite', attrs, name)
 
 
     def start_test(self, name, attrs):
-        self.send('start_test', name, attrs)
+        self.send('start_test', attrs, name)
 
 
     def start_keyword(self, name, attrs):
-        self.send('start_keyword', name, attrs)
+        self.send('start_keyword', attrs, name)
 
 
     def end_test(self, name, attrs):
-        self.send('end_test', name, attrs)
+        self.send('end_test', attrs, name)
 
 
     def end_suite(self, name, attrs):
-        self.send('end_suite', name, attrs)
+        self.send('end_suite', attrs, name)
+
+
+    def message(self, message):
+        self.send('log_message', message)
+
+
+    def log_message(self, message):
+        self.send('log_message', message)
 
 
     def close(self):
         print('\n## Closing WS connection.')
+        # leave a little time for the server to finish reading the data.
+        time.sleep(0.3)
         self.ws.close()
 
 
-    def send(self, msgType, name, attrs):
-        attrs['msgType'] = msgType
-        attrs['name'] = name
-        data = json.dumps(attrs, sort_keys=True, indent=4, separators=(',', ': ')) 
+    def send(self, msgType, dict, name = None):
+        dict['msgType'] = msgType
+        if name is not None:
+            dict['name'] = name
+        data = json.dumps(dict, sort_keys=True, indent=4, separators=(',', ': ')) 
         print("\n## Sending : \n %s" % data)
         self.ws.send(data)
